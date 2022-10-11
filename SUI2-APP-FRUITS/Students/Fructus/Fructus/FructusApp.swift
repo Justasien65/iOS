bacf7119886d80f35e0bea4656e1ad8257ab9d3f
Edward "Roy" Welborn <45338915+EdwardWelborn@users.noreply.github.com>
2022-10-10 23:10:18 -0500

    Create New Program
    Create new program called FRUCTUS.
    Added a few resources to the folders

---
+++ SUI2-APP-FRUITS/Students/Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Roy Welborn on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
