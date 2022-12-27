7245ee3a976de89d5ec842433dcb5e798581b2ee
Edward "Roy" Welborn <45338915+EdwardWelborn@users.noreply.github.com>
2022-12-27 14:32:59 -0600

    Adding Next Section
    Adding next section to the Fructus application from the class

---
+++ SUI2-APP-FRUITS/Students/Fructus/Fructus/App/FruitDetailView.swift
@@ -0,0 +1,66 @@
+//
+//  FruitDetailView.swift
+//  Fructus
+//
+//  Created by Roy Welborn on 12/15/22.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    
+    // MARK: PROPERTIES
+    var fruit: Fruit
+    
+    // MARK: BODY
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(alignment: .center, spacing: 20) {
+                    
+                    VStack(alignment: .leading, spacing: 20) {
+                        
+                        // HEADLINE
+                        FruitHeaderView(fruit: fruit)
+                        
+                        // TITLE
+                        Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(fruit.gradientColor[1])
+                        
+                        //NUTRIENTS
+                        FruitNutrientsView(fruit: fruit)
+                        
+                        // SUBHEADLINE
+                        Text("Lean More About \(fruit.title)")
+                            .fontWeight(.bold)
+                            .foregroundColor(fruit.gradientColor[1])
+
+                        // DESCRIPTION
+                        Text(fruit.description)
+                            .multilineTextAlignment(.leading)
+                        // LINK
+                        SourceLinkView()
+                            .padding(.top, 10)
+                            .padding(.bottom, 40)
+                        
+                    } //: VSTACK
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: 640, alignment: .center)
+                }//: VSTACK
+                .navigationBarTitle(fruit.title, displayMode: .inline)
+                .navigationBarHidden(true)
+            }//: SCROLLVIEW
+            .edgesIgnoringSafeArea(.top)
+        }//: NAVIGATION VIEW
+    }//: BODY
+}
+
+// MARK: PREVIEW
+struct FruitDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitDetailView(fruit: fruitsData[0])
+            .previewDevice("iPhone 12 Pro")
+    }
+}
---
+++ SUI2-APP-FRUITS/Students/Fructus/Fructus/View/SourceLinkView.swift
@@ -0,0 +1,38 @@
+//
+//  SourceLinkView.swift
+//  Fructus
+//
+//  Created by Roy Welborn on 12/20/22.
+//
+
+import SwiftUI
+
+struct SourceLinkView: View {
+    
+    //MARK: PROPERTIES
+    
+    //MARK: BODY
+    var body: some View {
+        
+        GroupBox() {
+            HStack {
+                Text("Content Source")
+                Spacer()
+                Link("Wikipedia", destination: URL(string: "https://wikipedia.com")!)
+                Image(systemName: "arrow.up.right.square")
+                
+            }//: HSTACK
+            .font(.footnote)
+            
+        } //: GROUPBOX
+    }
+}
+
+//MARK: PREVIEW
+struct SourceLinkView_Previews: PreviewProvider {
+    static var previews: some View {
+        SourceLinkView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
