b74c4b4317c3e54ba503c4ed88cdbffbd8187aed
Edward "Roy" Welborn <45338915+EdwardWelborn@users.noreply.github.com>
2023-01-05 11:05:04 -0600

    Creating New Application
    Creating Africa Application from my Udemy Classwork

---
+++ SWIFTUI-APP-AFRICA-UPDATED/Students/Africa/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Roy Welborn on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
