e568b300b00e39dc603981c229c15cf629fe088e
Edward "Roy" Welborn <45338915+EdwardWelborn@users.noreply.github.com>
2023-01-10 14:09:10 -0600

    Create Africa Project
    Create africa project from udemy class

---
+++ SWIFTUI-APP-AFRICA-UPDATED/Students/Africa/Africa/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by Roy Welborn on 1/5/23.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Videos")
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
