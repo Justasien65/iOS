c9d3199b4512a4f7924aaa6a02bcbd02db0e5fff
Edward "Roy" Welborn <45338915+EdwardWelborn@users.noreply.github.com>
2023-07-13 18:46:41 -0500

    Adding files to the repository
    Adding video files as well as video tab

--- SWIFTUI-APP-AFRICA-UPDATED/Students/Africa/Africa/App/VideoListView.swift
+++ SWIFTUI-APP-AFRICA-UPDATED/Students/Africa/Africa/App/VideoListView.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct VideoListView: View {
+    // MARK: - Properties
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+    
+    // MARK: - Body
     var body: some View {
-        Text("Videos")
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                    VideoListItemView(video: item)
+                        .padding(.vertical, 8)
+                }//: Loop
+            }//: End List
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        // shuffle videos
+                        videos.shuffle()
+                        hapticImpact.impactOccurred()
+                    })
+                    {
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                }
+            }
+        }//: End NavigationView
     }
 }
 
+// MARK: - Preview
 struct VideoListView_Previews: PreviewProvider {
     static var previews: some View {
         VideoListView()
