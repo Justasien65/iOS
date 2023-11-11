6576c32f6bd1430b3276694683cedad1a5477606
Roy Welborn <erwelborn@student.fullsail.edu>
2023-11-11 14:45:29 -0500

    Merging iOS repositories

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var myNumber = 5*4
+print(myNumber)
+
+10*5
+
+
+// variables and constants
+
+var userName = "Hatfield"
+
+userName = "James"
+
+
+print(userName)
+
+userName = "Lars"
+
+print(userName)
+
+let userAge = 55.0
+let pi = 3.14
+userAge * pi
+
+
